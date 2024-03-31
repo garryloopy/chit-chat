65799170b4143071c18e9f3c3bdde3932a0aab43
Garry Jr Dayag <garrydayagjr@gmail.com>
2024-03-31 14:31:20 -0600

    Add application shell

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,119 @@
+"use client";
+
 import Image from "next/image";
 
-export default function Home() {
+import ProfileTest from "@/public/images/ricardo.jpg";
+import {
+  IoEllipsisHorizontalSharp,
+  IoChevronUpSharp,
+  IoSendSharp,
+} from "react-icons/io5";
+
+import { useState } from "react";
+
+import { FormEvent, ChangeEvent } from "react";
+
+export default function Home(): JSX.Element {
+  // State for more options in chat
+  const [toggleMoreOptions, setToggleMoreOptions] = useState(false);
+
+  // State for the current chat value
+  const [chatValue, setChatValue] = useState("");
+
+  /**
+   * Handler for toggling more options button found on chat container
+   */
+  const handleOnMoreOptionsClick = () => {
+    setToggleMoreOptions(!toggleMoreOptions);
+  };
+
+  /**
+   * Handler when a more options selection is clicked
+   */
+  const handleOnMoreOptionsSelectionClick = () => {
+    setToggleMoreOptions(false);
+  };
+
+  /**
+   * Handler for chat submission
+   * @param event The form event upon submission
+   */
+  const handleOnChatSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  /**
+   * Handler for value change in chat
+   * @param event The event upon a value change
+   */
+  const handleOnChatValueChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const val = event.target.value;
+    setChatValue(val);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+    <main className="h-screen w-full bg-stone-950 p-4">
+      {/* Chat container  */}
+      <div className="flex h-full w-full flex-col overflow-hidden rounded-xl bg-stone-700">
+        {/* Chat header  */}
+        <div className="relative flex h-24 w-full items-center justify-between bg-stone-900 px-4">
+          {/* Profile image and name  */}
+          <div className="flex flex-row items-center gap-4">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src={ProfileTest}
+              width={64}
+              height={64}
+              alt="Profile picture"
+              className="rounded-full"
             />
-          </a>
+            <p className="text-2xl font-semibold text-stone-300">
+              Ricardo Milos
+            </p>
+          </div>
+          {/* More options button  */}
+          <button
+            className="relative rounded-md p-2 text-stone-300 hover:bg-stone-800 active:bg-stone-700"
+            onClick={handleOnMoreOptionsClick}
+          >
+            {toggleMoreOptions ? (
+              <IoChevronUpSharp size={40} className="h-full w-full" />
+            ) : (
+              <IoEllipsisHorizontalSharp size={40} className="h-full w-full" />
+            )}
+          </button>
+
+          {/* More options  */}
+          <div
+            className={`absolute right-0 top-[84px] min-w-96 rounded-b-lg border-2 border-stone-900 bg-stone-950 p-4 text-stone-300 shadow-md ${toggleMoreOptions ? "pointer-events-auto opacity-100" : "pointer-events-none opacity-0"} transition-opacity duration-300`}
+          >
+            <button
+              className="h-12 w-full hover:bg-stone-800 active:bg-stone-700"
+              onClick={handleOnMoreOptionsSelectionClick}
+            >
+              Delete chat
+            </button>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        {/* Chat body  */}
+        <div className="h-full w-full"></div>
+        {/* Chat footer  */}
+        <form
+          className="relative h-24 w-full bg-stone-900 p-5"
+          onSubmit={handleOnChatSubmit}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <input
+            type="text"
+            onChange={handleOnChatValueChange}
+            placeholder="Message"
+            value={chatValue}
+            className="h-full w-full rounded-lg bg-stone-500 px-4 text-lg text-stone-100 outline-none transition-shadow duration-300 placeholder:text-stone-100 focus:ring focus:ring-stone-400"
+          />
+          <div className="pointer-events-none absolute inset-0 m-5 flex items-center justify-end">
+            <div className="pointer-events-auto rounded-md p-2 px-4">
+              <IoSendSharp size={24} className="h-full w-full" />
+            </div>
+          </div>
+        </form>
       </div>
     </main>
   );
