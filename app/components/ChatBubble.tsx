b0506cfec87551ee049d2aa2418f9fef403a8635
Garry Jr Dayag <garrydayagjr@gmail.com>
2024-04-08 14:00:31 -0600

    Fix chat bubble align issue

--- app/components/ChatBubble.tsx
+++ app/components/ChatBubble.tsx
@@ -1,17 +1,16 @@
-import { StaticImageData } from "next/image";
 import Image from "next/image";
 
 type ChatBubbleData = {
   owner: boolean;
-  image: StaticImageData;
-  children: React.ReactNode;
+  image: string;
+  contents: React.ReactNode;
 };
 
-export default function ChatBubble({ owner, image, children }: ChatBubbleData) {
+export default function ChatBubble({ owner, image, contents }: ChatBubbleData) {
   return (
     <div
       data-owner={owner}
-      className="flex w-full flex-row items-start gap-3 data-[owner=false]:mr-auto data-[owner=true]:ml-auto data-[owner=true]:flex-row-reverse md:w-8/12 lg:w-7/12 xl:w-6/12"
+      className="flex w-full flex-row items-center gap-3 data-[owner=false]:mr-auto data-[owner=true]:ml-auto data-[owner=true]:flex-row-reverse md:w-8/12 lg:w-7/12 xl:w-6/12"
     >
       <Image
         src={image}
@@ -24,7 +23,7 @@ export default function ChatBubble({ owner, image, children }: ChatBubbleData) {
         data-owner={owner}
         className="rounded-3xl p-8 data-[owner=false]:bg-stone-800 data-[owner=true]:bg-stone-400 data-[owner=true]:text-stone-900"
       >
-        {children}
+        {contents}
       </p>
     </div>
   );
